141d55a2d888410dc8a619952006cd662446d57d
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2021-06-03 12:41:26 +0100

    2nd commit

--- GameOfLife/GameOfLife.py
+++ GameOfLife/GameOfLife.py
@@ -4,22 +4,14 @@ def ask(msg):
     print(msg)
     return input()
 
+placement_mode = namedtouple("operation_mode", "mode_name mode_entry_point")
+
+placement_modes = { placement_mode("manual-placement", "placement-function-1"), placement_mode("random-placement", "placement-function-2") }
+
+placement_mode = ask("")
+
 named_characters = namedtuple("named_chars", "block_double empty_double")
 named_chars = named_characters("\u2588\u2588", "\u2591\u2591")
 
 rows = ask("how many rows?")
 cols = ask("how many columns?")
-
-operation_mode = namedtouple("operation_mode", "mode_name mode_entry_point")
-
-operation_modes = { operation_mode() }
-operation_mode = ask("how would you like to run? " + " ".join(operation_modes) )
-
-
-
-exit()
-for i in range(10):
-    outp += named_chars.block_double
-    outp += named_chars.empty_double
-
-print(outp)
