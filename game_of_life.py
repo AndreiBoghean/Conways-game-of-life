e0e2b4baf9293759d5c44efa8813bfa53cd416c8
AndreiBoghean <andreiboghean00@gmail.com>
2021-07-02 00:09:15 +0100

    fundamental work on modes

--- game_of_life.py
+++ game_of_life.py
@@ -4,14 +4,20 @@ def ask(msg):
     print(msg)
     return input()
 
-placement_mode = namedtouple("operation_mode", "mode_name mode_entry_point")
+def test1():
+    print("test1")
+    
+def test2():
+    print("test2")
 
-placement_modes = { placement_mode("manual-placement", "placement-function-1"), placement_mode("random-placement", "placement-function-2") }
+placement_names = ["manual", "random"]
+palcement_functions = [ test1, test2]
+placement_mode = ask(f"what placement mode would you like to use? {' '.join(placement_names)}")
+print(type(placement_mode))
+print(f"placement mode: {int(placement_mode)}")
 
-placement_mode = ask("test")
+#named_characters = namedtuple("named_chars", "block_double empty_double")
+#named_chars = named_characters("\u2588\u2588", "\u2591\u2591")
 
-named_characters = namedtuple("named_chars", "block_double empty_double")
-named_chars = named_characters("\u2588\u2588", "\u2591\u2591")
-
-rows = ask("how many rows?")
-cols = ask("how many columns?")
+#rows = ask("how many rows?")
+#cols = ask("how many columns?")
