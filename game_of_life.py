a401181549f34658ec4749ac91be9ccf819e17eb
Andrei Boghean <andreiboghean00@gmail.com>
2021-07-02 01:46:35 +0100

    working mode selection

--- game_of_life.py
+++ game_of_life.py
@@ -11,10 +11,11 @@ def test2():
     print("test2")
 
 placement_names = ["manual", "random"]
-palcement_functions = [ test1, test2]
-placement_mode = ask(f"what placement mode would you like to use? {' '.join(placement_names)}")
-print(type(placement_mode))
-print(f"placement mode: {int(placement_mode)}")
+placement_functions = [ test1, test2]
+placement_dict = dict(zip(placement_names, placement_functions))
+placement_mode = ask(f"what placement mode would you like to use? ({', '.join(placement_names)})")
+placement_dict[placement_mode]()
+
 
 #named_characters = namedtuple("named_chars", "block_double empty_double")
 #named_chars = named_characters("\u2588\u2588", "\u2591\u2591")
