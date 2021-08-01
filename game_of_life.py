a20df6773c1bf86e86adee57b615f276133ee22e
AndreiBoghean <andreiboghean00@gmail.com>
2021-08-01 13:47:01 +0100

    conway functionality implemented

--- game_of_life.py
+++ game_of_life.py
@@ -32,74 +32,71 @@ def entry(stdscr):
         
         display_chars = dict(zip(
             ["block_double", "empty_double"],
-            ["\u2588\u2588"+"\u2588\u2588", "\u2591\u2591"+"\u2591\u2591"]
+            ["\u2588\u2588", "\u2591\u2591"]
         ))
         
         grid = grid.astype(str)
         
-        grid[grid=="0"]=display_chars["empty_double"]+display_chars["empty_double"]
-        grid[grid=="1"]=display_chars["block_double"]+display_chars["block_double"]
+        grid[grid=="0"]=display_chars["empty_double"]
+        grid[grid=="1"]=display_chars["block_double"]
         
-        print(grid)
-        for i in range(grid.shape[0]):
-            line = ""
-            for ii in grid[i]:
-                line += f"{ii}{ii}"
-            write_line(line, 1)
+        for i in grid:
+            write_line("".join(i), 1)
                     
     def manual_placement(grid):
         write_line("unimplemented manual_placement()")
            
     def random_placement(grid):
         
-        randoms = np.random.choice(["0", "0", "1"], grid.size)
+        randoms = np.random.choice([0, 0, 1], grid.size)
         grid = np.reshape(randoms, grid.shape)
         
         return grid
     
     def conways(grid):
-        grid = grid.astype(int)
-        new_grid = np.zeros(np.shape(grid))
+        #grid = grid.astype(int)
+        new_grid = np.zeros(np.shape(grid)).astype(int)
 
         for y in range(np.shape(new_grid)[0]):
             for x in range(np.shape(new_grid)[1]):
-                print([*range(np.shape(new_grid)[0])])
-                print(f"[{x}, {y}] = [{new_grid[y, x]}, {grid[y, x]}]")
                 
-                tempX = x
-                if tempX+1 == np.shape(new_grid)[1]:
+                #obsolete debugging code
+                #print([*range(np.shape(new_grid)[0])])
+                #print(f"[{x}, {y}] = [{new_grid[y, x]}, {grid[y, x]}]")
+                
+                tempX = x+1
+                if tempX == np.shape(new_grid)[1]:
                     tempX = 0
                 
-                tempY = y
-                if tempY+1 == np.shape(new_grid)[0]:
+                tempY = y+1
+                if tempY == np.shape(new_grid)[0]:
                     tempY = 0
                 
-                
-                write(f"{grid[y-1, x-1]=} | {grid[y-1, x]=} | {grid[y-1, tempX]=}")
-                write_line(f"{grid[y, x-1]=} | {grid[y, tempX]=}")
-                write_line(f"{grid[tempY, x-1]=} | {grid[tempY, x]=} | {grid[tempY, tempX]=}")
-                write_line("|")
-                
-                 
-                new_grid[y, x] = (
+                neighbours = (
                 grid[y-1, x-1]+grid[y-1, x]+grid[y-1, tempX]+
                 grid[y, x-1]+grid[y, tempX]+
                 grid[tempY, x-1]+grid[tempY, x]+grid[tempY, tempX]
                 )
-        
-        write_line("new_grid:")
+                
+                if neighbours == 2:
+                    new_grid[y, x] = grid[y, x]
+                elif neighbours == 3:
+                    new_grid[y, x] = 1
+                
+        return new_grid
+        """
         for i in range(new_grid.shape[0]):
             line = ""
             for ii in new_grid[i]:
                 line += f"{ii}|"
             write_line(line, 1)
+        """
                 
     placement_dict = dict(zip(
         ["manual", "random"],
         [manual_placement, random_placement]
         ))
     
-    curses.noecho()
     
     if not curses.has_colors():
         write_line("your console does not support colour, some text may be unreadable")
@@ -116,8 +113,25 @@ def entry(stdscr):
     
     grid = np.zeros((rows, cols))
     grid = placement_dict[placement_mode](grid)
+    
+    iterr_no = 0
+    
+    write_line(f"itteration number: {iterr_no}", 1)
+    iterr_no+=1
+    
+    grid = conways(grid)
     render_grid(grid)
-    conways(grid)
+    
+    while True:
+        
+        write_line(f"itteration number: {iterr_no}", 1)
+        iterr_no+=1
+    
+        grid = conways(grid)
+        render_grid(grid)
+    
+        if ask("would you like to render another step?")[0] != "y":
+            break
     
     ask("press enter to exit program")
 curses.wrapper(entry)
